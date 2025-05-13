a9e43cfb2f5336e4a7d9e9f2f8c48c7ad6aba80c
benj-lazaro <benjie.work@gmail.com>
2025-05-13 16:44:30 +0800

    16 exercise 21 our first conditional exercise

---
+++ 16-javascript-decision-making/09-exercise-21/code/app.js
@@ -0,0 +1,12 @@
+function isEven(num) {
+  //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓
+
+  if (num % 2 === 0) {
+    console.log("even");
+  }
+  //AND THIS LINE ↑↑↑↑↑
+}
+
+let num = Math.floor(Math.random() * 10) + 1;
+
+isEven(num);
