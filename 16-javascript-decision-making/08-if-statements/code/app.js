fea732f474ab1be584ac9c76a1f87681583ff66f
benj-lazaro <benjie.work@gmail.com>
2025-05-13 12:45:20 +0800

    08 if statements

---
+++ 16-javascript-decision-making/08-if-statements/code/app.js
@@ -0,0 +1,11 @@
+let random = Math.random();
+
+if (random < 0.5) {
+  console.log("Random number is less than 0.5");
+  console.log(random);
+}
+
+if (random >= 0.5) {
+  console.log("Random number is greater than or equal to 0.5");
+  console.log(random);
+}
