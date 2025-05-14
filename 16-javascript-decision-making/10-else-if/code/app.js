b6c000fbd82ed4d1869732f7ee182e33fa7a557e
benj-lazaro <benjie.work@gmail.com>
2025-05-14 22:35:55 +0800

    10 else if

---
+++ 16-javascript-decision-making/10-else-if/code/app.js
@@ -0,0 +1,19 @@
+const dayOfWeek = "Monday";
+if (dayOfWeek === "Monday") {
+  console.log("Ugh, I hate Mondays!");
+} else if (dayOfWeek === "Saturday") {
+  console.log("Date night. Sa. Tur. Day. Night!");
+} else if (dayOfWeek === "Friday") {
+  console.log("Thank God, it's Friday!");
+}
+
+const age = 50;
+if (age < 5) {
+  console.log("You are a baby, so free of charge");
+} else if (age < 10) {
+  console.log("You are a child, pay $10.00");
+} else if (age < 65) {
+  console.log("You are an adult, pay $20.00");
+} else if (age > 66) {
+  console.log("You are a senior, pay $10.00");
+}
