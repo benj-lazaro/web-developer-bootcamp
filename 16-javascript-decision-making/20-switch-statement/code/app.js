6bbdb109bb76efa4fd5ec9a6cccfec1dac836ec0
benj-lazaro <benjie.work@gmail.com>
2025-05-16 18:27:28 +0800

    20 switch statement

---
+++ 16-javascript-decision-making/20-switch-statement/code/app.js
@@ -0,0 +1,30 @@
+// Switch Statement
+
+const day = 1;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thrusday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Weekend");
+    break;
+  case 7:
+    console.log("Weekend");
+    break;
+  default:
+    console.log("I don't know that day");
+    break;
+}
