4f5a3cf77d11c4bc5e3d1dd1988b95896677a495
benj-lazaro <benjie.work@gmail.com>
2025-05-20 01:35:17 +0800

    07 shift & unshift

---
+++ 17-javascript-arrays/07-shift-&-unshift/code/app.js
@@ -0,0 +1,18 @@
+// Array Methods Shift & Unshift
+
+let movieLine = ["tom", "nancy", "oliver", "eva"];
+console.log(movieLine);
+
+let shiftedPerson = movieLine.shift();
+console.log(movieLine);
+console.log(shiftedPerson);
+
+shiftedPerson = movieLine.shift();
+console.log(movieLine);
+console.log(shiftedPerson);
+
+movieLine.push("jerry", "deniz", "kevin", "carly");
+console.log(movieLine);
+
+importantPerson = movieLine.unshift("VIP");
+console.log(movieLine);
