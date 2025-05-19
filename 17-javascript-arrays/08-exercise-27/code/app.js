750e17384c0db8a0d418950a1f6ce411604e8521
benj-lazaro <benjie.work@gmail.com>
2025-05-20 01:41:37 +0800

    08 exercise 27 push, pop, shift & unshift practice

---
+++ 17-javascript-arrays/08-exercise-27/code/app.js
@@ -0,0 +1,10 @@
+const planets = ["The Moon", "Venus", "Earth", "Mars", "Jupiter"]; //DO NOT TOUCH THIS LINE!
+
+// YOUR CODE GOES BELOW THIS LINE:
+console.log(planets);
+
+planets.shift();
+planets.push("Saturn");
+planets.unshift("Mercury");
+
+console.log(planets);
