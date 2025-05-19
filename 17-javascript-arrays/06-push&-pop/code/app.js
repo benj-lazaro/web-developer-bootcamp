ab1a62cba17af2b1bcf8373554e7380217958556
benj-lazaro <benjie.work@gmail.com>
2025-05-19 21:41:05 +0800

    06 push & pop

---
+++ 17-javascript-arrays/06-push&-pop/code/app.js
@@ -0,0 +1,24 @@
+// Array Methods
+
+let movieLine = ["tom", "nancy"];
+console.log(movieLine);
+movieLine.push("pablo");
+movieLine.push("oliver");
+movieLine.push("eva");
+movieLine.push("harry", "hermione");
+console.log(movieLine);
+
+let removedPerson = movieLine.pop();
+console.log(movieLine);
+console.log(removedPerson);
+
+let barbell = [];
+barbell.push(45);
+barbell.push(45);
+barbell.push(25);
+barbell.push(10);
+barbell.push(2.5);
+console.log(barbell);
+
+barbell.pop();
+console.log(barbell);
