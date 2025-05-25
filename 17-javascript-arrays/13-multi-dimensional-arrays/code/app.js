885ce662c8a0f98c7c12814ae59fc884802c96c4
benj-lazaro <benjie.work@gmail.com>
2025-05-25 21:54:16 +0800

    13 multi-dimensional arrays

---
+++ 17-javascript-arrays/13-multi-dimensional-arrays/code/app.js
@@ -0,0 +1,12 @@
+// Multi-Dimensional Arrrays
+
+const gameBoard = [
+  ["X", "O", "X"],
+  ["O", null, "X"],
+  ["O", "O", "X"],
+];
+
+console.log(gameBoard);
+
+// Accessing the "null" value inside a multi-dimensional array
+console.log(gameBoard[1][1]);
