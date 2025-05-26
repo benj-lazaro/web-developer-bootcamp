fcf11a69652eb9a546def60b0a6e6a8f41592976
benj-lazaro <benjie.work@gmail.com>
2025-05-26 22:01:56 +0800

    03 creating object literals

---
+++ 18-javascript-object-literals/03-creating-object-literals/code/app.js
@@ -0,0 +1,20 @@
+// Creating Object Literals
+
+// Object literal with properties of type string
+const person = {
+  firstName: "Mick",
+  lastName: "Jagger",
+};
+
+// Access the object literal "person"
+console.log(person);
+
+// Object literal with different types of properties
+const kitchenSink = {
+  faveNum: 93219023,
+  isFunny: true,
+  colors: ["red", "orange"],
+};
+
+// Access the object literal "kitchenSink"
+console.log(kitchenSink);
