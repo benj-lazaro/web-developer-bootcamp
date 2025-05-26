95030dd4a33457850755bca3b2df4f243ad345e0
benj-lazaro <benjie.work@gmail.com>
2025-05-26 17:10:24 +0800

    02 introducing object literals

---
+++ 18-javascript-object-literals/02-intro-object-literals/code/app.js
@@ -0,0 +1,12 @@
+// Introducting to Object Literals
+
+const fitbitData = {
+  totalSteps: 308727,
+  totalMiles: 211.7,
+  avgCalorieBurn: 5755,
+  workoutThisWeek: "5 or 7",
+  avgGoodSleep: "2:13",
+};
+
+console.log("Total Steps: " + fitbitData.totalSteps);
+console.log("Average Calorie Burn: " + fitbitData.avgCalorieBurn);
