5e14099fc9290b9adb3262d4ec234baa6c4566ce
benj-lazaro <benjie.work@gmail.com>
2025-05-27 17:22:53 +0800

    08 nesting arrays and objects

---
+++ 18-javascript-object-literals/08-nesting-arrays-objects/code/app.js
@@ -0,0 +1,19 @@
+// Nesting Arrays & Objects
+
+const comments = [
+  {
+    username: "Tammy",
+    text: "lolololol",
+    votes: 9,
+  },
+  {
+    username: "FishBoi",
+    text: "glub glub",
+    votes: 12387,
+  },
+];
+
+console.log(comments);
+
+// Accessing the 2nd element & it's property "text" using the dot notation
+console.log(comments[1].text);
