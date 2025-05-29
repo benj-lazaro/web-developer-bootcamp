e3cefd19906347c302369cbc4c3789f90381e647
benj-lazaro <benjie.work@gmail.com>
2025-05-29 17:21:15 +0800

    04 more for loops

---
+++ 19-repeating-stuff-w-loops/04-more-for-loops/code/app.js
@@ -0,0 +1,24 @@
+// More for Loops Examples
+
+// Print even numbers
+console.log("Even numbers");
+for (let index = 2; index <= 20; index += 2) {
+  console.log(index);
+}
+
+// Print odd numbers
+console.log("Odd numbers");
+for (let i = 1; i <= 20; i += 2) {
+  console.log(i);
+}
+
+// Count down from 100
+console.log("Count down from 100 to 0 by 10s");
+for (let num = 100; num >= 0; num -= 10) {
+  console.log(num);
+}
+
+console.log("From 10 to 1000, multiple each iteration by 10");
+for (let x = 10; x <= 1000; x *= 10) {
+  console.log(x);
+}
