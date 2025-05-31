f4b65052857416f09207d315271c468a992ed83f
benj-lazaro <benjie.work@gmail.com>
2025-05-31 22:57:09 +0800

    08 exercise 33 iterating arrays exercise

---
+++ 19-repeating-stuff-w-loops/08-exercise-33/code/app.js
@@ -0,0 +1,6 @@
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for (let index = 0; index < people.length; index++) {
+  console.log(people[index].toLocaleUpperCase());
+}
