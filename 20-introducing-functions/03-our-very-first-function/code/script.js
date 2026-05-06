82b09bea94c83df9ab553fe1064b31dc6d32ad57
benj-lazaro <benjie.work@gmail.com>
2026-05-06 13:23:33 +0800

    03 our very first function

---
+++ 20-introducing-functions/03-our-very-first-function/code/script.js
@@ -0,0 +1,14 @@
+// Defining a function
+function singSong() {
+  console.log("DO");
+  console.log("RE");
+  console.log("MI");
+}
+
+// Call / execute the function
+singSong();
+singSong();
+singSong();
+singSong();
+singSong();
+singSong();
