e1e78933f23d2a4f55a4c5e9b133b8f677b00faf
benj-lazaro <benjie.work@gmail.com>
2026-05-07 18:22:48 +0800

    05 arguments intro

---
+++ 20-introducing-functions/05-arguments-intro/code/app.js
@@ -0,0 +1,8 @@
+// Function w/ a parameter variable
+function greet(firstName) {
+  console.log(`Hey there, ${firstName}`);
+}
+
+// The values "Tim" & "Anya" are argument values
+greet("Tim");
+greet("Anyya");
