1d0557622e32030582627adf252afa008aa27aa0
benj-lazaro <benjie.work@gmail.com>
2026-05-10 16:32:55 +0800

    10 exercise 38 return value practice

---
+++ 20-introducing-functions/10-exercise-38/code/app.js
@@ -0,0 +1,15 @@
+// DEFINE YOUR FUNCTION BELOW:
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+// Test case
+let product;
+product = multiply(2, 3);
+console.log(product); // Returns the value of 6
+
+product = multiply(9, 9);
+console.log(product); // Returns the value of 81
+
+product = multiply(5, 4);
+console.log(product); // Returns the value of 20
