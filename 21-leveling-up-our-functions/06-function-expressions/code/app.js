21d4e315310fb20ea605b9bca7bee01b698b6e9c
benj-lazaro <benjie.work@gmail.com>
2026-05-16 23:16:21 +0800

    06 function expressions

---
+++ 21-leveling-up-our-functions/06-function-expressions/code/app.js
@@ -0,0 +1,12 @@
+// Function expression
+let square = function (num) {
+  return num * num;
+};
+
+console.log(square(7)); // Returns 49
+
+const add = function (x, y) {
+  return x + y;
+};
+
+console.log(add(3, 4)); // Returns 7
