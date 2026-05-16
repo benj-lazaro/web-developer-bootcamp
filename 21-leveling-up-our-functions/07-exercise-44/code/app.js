8f9376c65bf96ac77a796b49daa7150b1154497f
benj-lazaro <benjie.work@gmail.com>
2026-05-16 23:21:47 +0800

    07 exercise 44 function expressions exercise

---
+++ 21-leveling-up-our-functions/07-exercise-44/code/app.js
@@ -0,0 +1,8 @@
+//NOTE: Udemy's coding exercise platform does NOT support the ** operator.  You'll need to multiply a number by itself or use the Math.pow() method.
+let square = function (num) {
+  return num * num;
+};
+
+// Test case
+console.log(square(4)); // Returns 16
+console.log(square(3)); // Returns 9
