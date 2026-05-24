af0103dd6ffdb22190f0f83f73ad62dc5f92e830
benj-lazaro <benjie.work@gmail.com>
2026-05-24 16:54:19 +0800

    03 the foreach method

---
+++ 22-callback-array-methods/03-the-foreach-method/code/app.js
@@ -0,0 +1,24 @@
+// Array of numbers
+const nums = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+
+// forEach() method w/ an inline Anonymous function as callback
+nums.forEach(function (element) {
+  if (element % 2 === 0) {
+    console.log(element);
+  }
+});
+
+// Array of objects
+const movies = [
+  { title: "Amadeus", score: 99 },
+  { title: "Stand by me", score: 85 },
+  { title: "Parasite", score: 95 },
+  { title: "Alien", score: 90 },
+];
+
+// forEach() method w/ an inline Anonymous function as callback
+movies.forEach(function (movie) {
+  console.log(`${movie.title} - ${movie.score}/100`);
+});
