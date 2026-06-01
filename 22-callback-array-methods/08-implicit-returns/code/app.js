9976db65163e87a053359ee3aaf25b63acd77b26
benj-lazaro <benjie.work@gmail.com>
2026-06-01 22:36:56 +0800

    08 arrow function implicit returns

---
+++ 22-callback-array-methods/08-implicit-returns/code/app.js
@@ -0,0 +1,6 @@
+// Arrow functions w/   implicit return
+const rollDie = () => Math.floor(Math.random() * 6) + 1;
+console.log(rollDie());
+
+const add = (num1, num2) => num1 + num2;
+console.log(add(5, 5));
