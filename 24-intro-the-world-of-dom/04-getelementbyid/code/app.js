41015a71578fa1f9a55a9891e1b828e22ae9d9a0
benj-lazaro <benjie.work@gmail.com>
2026-07-07 16:09:04 +0800

    04 getelementbyid

---
+++ 24-intro-the-world-of-dom/04-getelementbyid/code/app.js
@@ -0,0 +1,13 @@
+// Select the DOM object w/ an "id" attribute of "banner"
+const banner = document.getElementById("banner");
+
+// View the selected DOM object's HTML representation
+console.log(banner);
+
+// View the selected DOM object's properties
+console.dir(banner);
+
+// Select the DOM object w/ an "id" attribute of "toc"
+const toc = document.getElementById("toc");
+console.log(toc);
+console.dir(toc);
