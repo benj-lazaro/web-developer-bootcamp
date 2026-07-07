5a464ccbcdc5bfb6c8c788bfe301d9e1f970e0a3
benj-lazaro <benjie.work@gmail.com>
2026-07-07 14:51:03 +0800

    03 the document object

---
+++ 24-intro-the-world-of-dom/03-the-document-object/code/app.js
@@ -0,0 +1,8 @@
+// View the properties of the "document" object
+console.dir(document);
+
+// View the ALL of the HTML elements in the web page as JavaScript objects in an array
+console.log(document.all);
+
+// To change the text of the 1st HTML element <b>
+document.all[10].innerText = "Sealkey";
