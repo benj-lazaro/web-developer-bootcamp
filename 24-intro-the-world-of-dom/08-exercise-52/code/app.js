ff4d10c1e0eef9013b15f186580e0d8dc4100989
benj-lazaro <benjie.work@gmail.com>
2026-07-08 16:10:20 +0800

    08 exercise 52 queryselector practice

---
+++ 24-intro-the-world-of-dom/08-exercise-52/code/app.js
@@ -0,0 +1,7 @@
+// Write your code in here:
+let doneTodos = document.querySelectorAll(".done");
+let checkbox = document.querySelector("input:checked");
+
+// Test case
+console.log(doneTodos);
+console.log(checkbox);
