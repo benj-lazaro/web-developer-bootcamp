a8a86e377d35c5a39516681b8972dfc906e22c2e
benj-lazaro <benjie.work@gmail.com>
2026-07-10 20:03:15 +0800

    12 exercise 54 manipulating attributes practice code & content refactor

--- 24-intro-the-world-of-dom/12-exercise-54/code/app.js
+++ 24-intro-the-world-of-dom/12-exercise-54/code/app.js
@@ -1,10 +1,10 @@
+const image = document.querySelector("img");
+
 // Update the attribute "src"
-document
-  .querySelector("img")
-  .setAttribute(
-    "src",
-    "https://images.unsplash.com/photo-1581061090142-c2cd0ec9f021?w=200",
-  );
+image.setAttribute(
+  "src",
+  "https://images.unsplash.com/photo-1581061090142-c2cd0ec9f021?w=200",
+);
 
 // Update the attribute "alt"
-document.querySelector("img").setAttribute("alt", "chicken");
+image.setAttribute("alt", "chicken");
