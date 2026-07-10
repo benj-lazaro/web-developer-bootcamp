cd09901e51483007a59495d904df6bc1675554b5
benj-lazaro <benjie.work@gmail.com>
2026-07-10 19:49:54 +0800

    12  exercise 54 manipulating attributes practice

---
+++ 24-intro-the-world-of-dom/12-exercise-54/code/app.js
@@ -0,0 +1,10 @@
+// Update the attribute "src"
+document
+  .querySelector("img")
+  .setAttribute(
+    "src",
+    "https://images.unsplash.com/photo-1581061090142-c2cd0ec9f021?w=200",
+  );
+
+// Update the attribute "alt"
+document.querySelector("img").setAttribute("alt", "chicken");
